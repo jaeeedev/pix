2ae5ba7b0126b048f5b68cfe59d54a0060bfde41
Jaeee <wldms1107@gmail.com>
2023-05-17 00:20:47 +0900

    feat: 프로젝트 생성
    - 라이브러리 설치
    - provider 래핑
    
    close #1

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { RecoilRoot } from "recoil";
+import { RouterProvider } from "react-router-dom";
+import router from "./router.ts";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </React.StrictMode>
+);
