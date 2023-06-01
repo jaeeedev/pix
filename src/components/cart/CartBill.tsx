59ab3028151431206529545fa41a116657c1f054
Jaeee <wldms1107@gmail.com>
2023-06-01 17:57:31 +0900

    refactor: CartBill 코드 정리

--- src/components/cart/CartBill.tsx
+++ src/components/cart/CartBill.tsx
@@ -1,5 +1,7 @@
 import Children from "../../types/children";
 import { CartData } from "../../types/cart";
+import Button from "../common/Button";
+import { twMerge } from "tailwind-merge";
 
 const BillTextWrapper = ({ children }: Children) => {
   return <div className="flex justify-between py-2">{children}</div>;
@@ -16,27 +18,41 @@ const CartBill = ({ cartData }: Props) => {
   return (
     <div className="w-full m-0 md:flex-[1] p-4 rounded-md border border-solid border-slate-200 h-fit">
       <BillTextWrapper>
-        <span className="font-medium text-slate-400 text-lg">상품 합계</span>
-        <span className="font-medium text-lg">{cartSum.toLocaleString()}</span>
+        <Span light="on">상품 합계</Span>
+        <Span>{cartSum.toLocaleString()}</Span>
       </BillTextWrapper>
       <BillTextWrapper>
-        <span className="font-medium text-slate-400 text-lg">배송비</span>
-        <span className="font-medium text-slate-400 text-lg">
-          {fee.toLocaleString()}
-        </span>
+        <Span light="on">배송비</Span>
+        <Span light="on">{fee.toLocaleString()}</Span>
       </BillTextWrapper>
       <hr />
 
       <div className="flex justify-between py-4">
-        <span className="font-medium text-lg">total</span>
-        <span className="font-medium text-xl">{total}</span>
+        <Span>total</Span>
+        <Span extend="text-xl">{total}</Span>
       </div>
 
-      <button className="w-full p-3 bg-slate-800 rounded-md text-white">
-        구매하기
-      </button>
+      <Button full="on">구매하기</Button>
     </div>
   );
 };
 
 export default CartBill;
+
+type TSpan = Children & {
+  light?: "on" | "off";
+  extend?: string;
+};
+
+const Span = ({ children, light, extend }: TSpan) => {
+  return (
+    <span
+      className={twMerge(
+        `font-medium text-lg  ${light === "on" && "text-slate-400"}`,
+        extend
+      )}
+    >
+      {children}
+    </span>
+  );
+};
